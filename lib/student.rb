3cb1c63c6c15b0994cbba51399e7c3f204fe23f8
Oscar Shoudt <shoudto@gmail.com>
2019-09-03 18:21:34 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -53,12 +53,15 @@ end
   end
 
   def self.new_from_db(row)
-    new_student = self.new(row[1], row[2], row[0])
+    new_student = self.new(row[1], row[2], row[0]) # research more about this line
     new_student
   end
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
+    sql = <<-SQL
+      SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[1], result[2], result[0])
   end
