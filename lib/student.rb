153dd15865f2fa247ca2d492008adf17bde8b2db
Oscar Shoudt <shoudto@gmail.com>
2019-09-03 18:19:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ end
   end
 
   def self.new_from_db(row)
-    new_student = self.new(row[1], row[2], row[0])
+    new_student = self.new(row[1], row[2], row[0]) # research more about this line 
     new_student
   end
 
